543af73a8664246a286207258f76d81f31c7951d
rinatsharifullin <58179621+rinatsharifullin@users.noreply.github.com>
2020-05-24 18:11:25 +0100

    Add files via upload

--- scripts/basescript.js
+++ scripts/basescript.js
@@ -8,24 +8,7 @@ $(document).ready(function(){
 		$('#navigation').slideToggle();
     });
 	
-    
-    // Click on arrow down on main page
-    if (document.location.href.match(/[^\/]+$/)[0] == 'index.html'){
-        var $doc = $(document), $win = $(window);
-        var $svg = $('#sticky svg').drawsvg();
-        $('.container').click(function(){    
-            max = $doc.height() - $win.height();
-            $('.container').css('bottom', -100 + 'px');	//Bring arrows down
-            $svg.drawsvg('animate');
-            $(window).off('scroll');
-        });
-        $win.on('scroll', function() {
-            $svg.drawsvg('animate');
-            $('.container').css('bottom', -100 + 'px');
-            $(window).off('scroll');
-        });
-    
-    };
+
 
     // Scroll top arrow
     $(window).scroll(function(){
