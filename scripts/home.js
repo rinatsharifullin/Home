ba5841b5f17a82de52c9f3173c4dfc70990ff634
rinatsharifullin <58179621+rinatsharifullin@users.noreply.github.com>
2020-05-24 17:39:20 +0100

    Add files via upload

---
+++ scripts/home.js
@@ -0,0 +1,21 @@
+/* This function provide waiting time till content loaded */
+
+$(document).ready(function(){
+    // Click on arrow down on main page
+    if (document.location.href.match(/[^\/]+$/)[0] == 'index.html'){
+        var $doc = $(document), $win = $(window);
+        var $svg = $('#sticky svg').drawsvg();
+        $('.container').click(function(){    
+            max = $doc.height() - $win.height();
+            $('.container').css('bottom', -100 + 'px');	//Bring arrows down
+            $svg.drawsvg('animate');
+            $(window).off('scroll');
+        });
+        $win.on('scroll', function() {
+            $svg.drawsvg('animate');
+            $('.container').css('bottom', -100 + 'px');
+            $(window).off('scroll');
+        });
+    
+    };
+});
